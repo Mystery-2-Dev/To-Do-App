42bec5d09b7b4ce3306933778a18cbe14286d0db
Mystery-2-dev <mystery2deve@gmail.com>
2022-02-01 03:23:00 +0530

    .

--- todo/components/TodoList.js
+++ todo/components/TodoList.js
@@ -1,6 +1,6 @@
 import { colors } from '@material-ui/core';
 import React from 'react'
-import {StyleSheet,Text,View} from 'react-native'
+import {StyleSheet,Text,TouchableOpacity,View,Modal} from 'react-native'
 import Colors from '../Colors';
 
 export default class TodoList extends React.Component{
@@ -16,7 +16,14 @@ export default class TodoList extends React.Component{
         const remainingCount=list.todos.length-completedCount;
     
         return(
-            <View style={[styles.listContainer,{backgroundColor:list.color}]}>
+            <View>
+                <Modal animationType="slide" visible={this.state.showListVisible} onRequestClose={() =>this.toggleListModal()}>
+                    <View>
+                        <Text>List Modal</Text>
+                    </View>
+                </Modal>
+                <TouchableOpacity style={[styles.listContainer,{backgroundColor:list.color}]}
+                onPress={() =>this.toggleListModal()}>
                 <Text style={styles.listTitle} numberOfLines={1}>
                     {list.name}
                 </Text>
@@ -31,6 +38,7 @@ export default class TodoList extends React.Component{
                  <Text style={styles.subtitle}>Completed</Text>
                 </View>
              </View>
+            </TouchableOpacity>
             </View>
         );
     }
